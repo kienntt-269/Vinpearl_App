fea0e13649939de6bca9d125db39e2b8225b1a13
Nguyễn Tiến Trung Kiên <99466207+kientee@users.noreply.github.com>
2023-05-25 21:52:24 +0700

    update vinpearl

--- api/account.js
+++ api/account.js
@@ -3,8 +3,8 @@ import axios from 'axios'
 import domain from './domain'
 
 const accountApi = {
-    login: (data) => axiosClient.post(`customer/login`, data),
-    register: (data) => axiosClient.post(`customer/register`, data),
+    login: (data) => axiosClient.post(`public/customer/login`, data),
+    register: (data) => axiosClient.post(`public/customer/register`, data),
     detail: (id) => axiosClient.get(`customer/detail/${id}`),
     update: (id, data) => axiosClient.put(`customer/update/${id}`, data),
 }
